40c34962e816ea39520db07dce802b254191221b
Anderson <anderson.fndss@gmail.com>
2023-08-09 08:51:03 -0300

    Validação dos campos sensíveis (not ready yet)

--- formulario.php
+++ formulario.php
@@ -8,8 +8,8 @@ use PHPMailer\PHPMailer\PHPMailer;
 
 session_start();
 
-// Verifica se o email foi enviado para exibir notificação de 
-if (isset($_SESSION['email_enviado']) && $_SESSION['email_enviado']) {
+// Verifica se o email foi enviado para exibir notificação de sucesso
+if (isset($_SESSION['email_enviado'])) {
     echo '<script>alert("Solicitação enviada com sucesso! Aguarde nosso retorno em breve.")</script>';
     // Limpa a variável de sessão após exibir a notificação
     unset($_SESSION['email_enviado']);
@@ -18,8 +18,7 @@ if (isset($_SESSION['email_enviado']) && $_SESSION['email_enviado']) {
 if(isset($_POST['submit']))
 {
   
-    
-    include_once('config.php');
+    include_once('conexao_bd.php');
 
     $nome = $_POST['nome'];
     $email = $_POST['email'];
@@ -31,14 +30,48 @@ if(isset($_POST['submit']))
     $assunto = $_POST ['assunto'];
     $descricao = $_POST ['descricao'];
 
-    $result = mysqli_query($conexao, "INSERT INTO formulario(nome,email,telefone,cpf_cnpj,data_nascimento,endereco,cep,assunto,descricao) 
-    VALUES ('$nome','$email','$telefone','$cpf_cnpj','$data_nascimento','$endereco','$cep', '$assunto', '$descricao')");
+    // Variável para verificar se todos os campos estão válidos
+    // Array para armazenar mensagens de erro
+    $erros = array();
 
-  // Verificação e envio do email
-  if($result){
+    // Validação de email
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $erros['email'] = 'Digite um email válido!';
+    } 
 
+    // Validação de cpf/cnpj
+    if (!preg_match("/[0-9]{3}\.[0-9]{3}\.[0-9]{3}\-[0-9]{2}/", $cpf_cnpj)) {
+        $erros['cpf_cnpj'] = 'Digite um CPF/CNPJ válido!';
+    } 
+
+    // Validação de telefone
+    if (!preg_match("/\([0-9]{2}\)[0-9]{4,5}\-[0-9]{4}/", $telefone)) {
+        $erros['telefone'] = 'Digite um telefone válido!';
+    } 
+
+    // Validação de cep
+    if (!preg_match("/[0-9]{5}\-[0-9]{3}/", $cep)) {
+        $erros['cep'] = 'Digite um CEP válido!';
+    }
+
+    // Inserção dos dados no banco de dados se não há erros
+    if (count($erros) == 0) {
+        $result = mysqli_query($conexao, "INSERT INTO formulario(nome,email,telefone,cpf_cnpj,data_nascimento,endereco,cep,assunto,descricao) 
+        VALUES ('$nome','$email','$telefone','$cpf_cnpj','$data_nascimento','$endereco','$cep', '$assunto', '$descricao')");
+
+    // Verificação e envio do email
+    if($result){
+
+    // Inicia a classe PHPMailer
     $mail = new PHPMailer();
+
+    $descricaoFinal = "Formulario do Contribuinte: " . PHP_EOL . "Nome: $nome" . PHP_EOL . "Email: $email" . PHP_EOL . 
+    "Telefone: $telefone" . PHP_EOL . "CPF/CNPJ: $cpf_cnpj" . PHP_EOL . "Data de Nascimento: $data_nascimento "
+         . PHP_EOL . "Endereco: $endereco" . PHP_EOL . "CEP: $cep" . PHP_EOL . "Assunto: $assunto" . PHP_EOL . 
+         "Descricao: $descricao";
+
     try {
+
         // Configurações do servidor SMTP externo
         $smtpHost = 'smtp-mail.outlook.com'; // Por exemplo, smtp.gmail.com
         $smtpPort = 587; // Porta do servidor SMTP (587 para TLS, 465 para SSL)
@@ -59,7 +92,7 @@ if(isset($_POST['submit']))
         $mail->setFrom('teste.sutec@outlook.com', $nome);
         $mail->addAddress($to);
         $mail->Subject = $assunto;
-        $mail->Body = $descricao;
+        $mail->Body = $descricaoFinal;
     
         // Envia o email
         $mail->send();
@@ -73,13 +106,13 @@ if(isset($_POST['submit']))
 
     } catch (Exception $e) {
         echo "Erro ao enviar o email: {$mail->ErrorInfo}";
-        
-        
     }
   }  
 }
- 
+}
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -102,18 +135,27 @@ if(isset($_POST['submit']))
                 </div>
                 <br><br>
                 <div class="inputBox">
-                    <input type="text" name="email" id="email" class="inputUser" required>
                     <label for="email" class="labelInput">Email:</label>
+                    <input type="text" name="email" id="email" class="inputUser" value="<?php echo isset($email) ? $email : ''; ?>" required>
+                    <?php if(isset($erros['email'])): ?>
+                        <div style="color:red;"><?php echo $erros['email']; ?></div>
+                    <?php endif; ?>
                 </div>
                 <br><br>
                 <div class="inputBox">
-                    <input type="tel" name="telefone" id="telefone" class="inputUser" required>
                     <label for="telefone" class="labelInput">Telefone:</label>
+                    <input type="tel" name="telefone" id="telefone" class="inputUser" value="<?php echo isset($telefone) ? $telefone : ''; ?>" required>
+                    <?php if(isset($erros['telefone'])): ?>
+                        <div style="color:red;"><?php echo $erros['telefone']; ?></div>
+                    <?php endif; ?>
                 </div>
                 <br><br>
                 <div class="inputBox">
-                    <input type="cpf_cnpj" name="cpf_cnpj" id="cpf_cnpj" class="inputUser" required>
                     <label for="cpf_cnpj" class="labelInput">CPF/CNPJ:</label>
+                    <input type="text" name="cpf_cnpj" id="cpf_cnpj" class="inputUser" value="<?php echo isset($cpf_cnpj) ? $cpf_cnpj : ''; ?>" required>
+                    <?php if(isset($erros['cpf_cnpj'])): ?>
+                        <div style="color:red;"><?php echo $erros['cpf_cnpj']; ?></div>
+                    <?php endif; ?>
                 </div>
                 <p></p>
                 <label for="data_nascimento"><b>Data de Nascimento:</b></label>
@@ -125,8 +167,11 @@ if(isset($_POST['submit']))
                 </div>
                 <br><br>
                 <div class="inputBox">
-                    <input type="decimal" name="cep" id="cep" class="inputUser" required>
                     <label for="cep" class="labelInput">CEP:</label>
+                    <input type="decimal" name="cep" id="cep" class="inputUser" value="<?php echo isset($cep) ? $cep : ''; ?>" required>
+                    <?php if(isset($erros['cep'])): ?>
+                        <div style="color:red;"><?php echo $erros['cep']; ?></div>
+                    <?php endif; ?>
                 </div>
                 <br><br>
                 <div class="inputBox">
